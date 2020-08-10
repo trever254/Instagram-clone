03531fa91d9dbb23e020782342c9773ee52ba29c
trever <toakumu@gmail.com>
2020-08-10 17:26:27 +0300

    Added models.py

--- inst/models.py
+++ inst/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
 
 # Create your models here.
+class Pic(models.Model):
+    pic = models.ImageField(upload_to = "pics/",null = True)
+    user = models.ForeignKey(User,null=True)
+    pic_name = models.CharField(max_length = 30,null = True)
+    likes = models.IntegerField(default=0)
+    pic_caption = models.TextField(null = True)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+    # profile = models.ForeignKey(Profile, null=True) 
+    comments = models.IntegerField(default=0)
+
+    def __str__(self):
+        	return self.pic_name
+
+    def delete_pic(self):
+    	self.delete()
+
+    def save_pic(self):
+    	self.save()
+
+    def update_caption(self,new_caption):
+    	self.pic_caption = new_caption
+    	self.save()
