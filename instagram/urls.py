087d1fa08c1377317019d89802cf38693681e7aa
trever <toakumu@gmail.com>
2020-08-10 13:22:06 +0300

    added changes on urls.py file

--- instagram/urls.py
+++ instagram/urls.py
@@ -20,4 +20,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'',include('inst.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
