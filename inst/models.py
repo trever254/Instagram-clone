88c704469991ff146b9b57a703c0664e338c7cb5
trever <toakumu@gmail.com>
2020-08-10 21:40:14 +0300

    added changes on models.py

--- inst/models.py
+++ inst/models.py
@@ -26,3 +26,92 @@ class Pic(models.Model):
     def update_caption(self,new_caption):
     	self.pic_caption = new_caption
     	self.save()
+
+
+    @classmethod
+    def get_pics_by_user(cls,id):
+        sent_pics = Pic.objects.filter(user_id=id)
+        return sent_pics
+
+    @classmethod
+    def get_pics_by_id(cls,id):
+        fetched_pic = Pic.objects.get(id = id)
+        return  fetched_pic
+
+    class Meta:
+    	ordering = ['-pub_date']
+
+
+    def __str__(self):
+    	return self.user.username
+
+    def save_profile(self):
+    	self.save()
+
+class Profile(models.Model):
+    username = models.CharField(default='User',max_length=30)
+    profile_pic = models.ImageField(upload_to = "profile/",null=True)
+    bio = models.TextField(default='',blank = True)
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+
+    def __str__(self):
+    	return self.username
+
+    def delete_profile(self):
+        self.delete()
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def search_profile(cls,search_term):
+        got_profiles = cls.objects.filter(first_name_icontains = search_term)
+        return got_profiles
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,null= True)
+    pic = models.ForeignKey(Pic, null=True,related_name='comment')
+    comment= models.TextField(blank=True)
+
+    def __str__(self):
+        return self.comment
+
+    def delete_comment(self):
+    		self.delete()
+
+    def save_comment(self):
+        self.save()
+
+class Follow(models.Model):
+    	user = models.ForeignKey(Profile,null=True)
+	follower = models.ForeignKey(User,null=True)
+
+	def __int__(self):
+		return self.name
+
+	def save_follower(self):
+		self.save()
+
+	def delete_follower(self):
+		self.save()
+
+class Unfollow(models.Model):
+    	user = models.ForeignKey(Profile,null=True)
+	follower = models.ForeignKey(User,null=True)
+
+	def __int__(self):
+		return self.name
+
+class Likes(models.Model):
+    	user = models.ForeignKey(Profile,null=True)
+	# pic = models.ForeignKey(Pic,null=True)
+
+	def __int__(self):
+		return self.name
+
+	def unlike(self):
+		self.delete()
+
+	def save_like(self):
+		self.save() 
